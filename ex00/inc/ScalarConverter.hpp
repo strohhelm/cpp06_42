77cee772bf25c241d700adbd15fa9f80d14615da
pstrohal <pstrohal@student.42heilbronn.de>
2025-01-07 16:27:52 +0100

    precision overengineer of doom

--- ex00/inc/ScalarConverter.hpp
+++ ex00/inc/ScalarConverter.hpp
@@ -6,7 +6,7 @@
 /*   By: pstrohal <pstrohal@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/19 22:08:49 by pstrohal          #+#    #+#             */
-/*   Updated: 2025/01/04 19:42:47 by pstrohal         ###   ########.fr       */
+/*   Updated: 2025/01/07 13:13:46 by pstrohal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,13 +42,13 @@ class ScalarConverter
 	private:
 		ScalarConverter();
 		~ScalarConverter();
-		static e_type	check_type(std::string input);
-		static e_type	isFloatOrDouble(std::string in);
+		static e_type	check_type(std::string input, int &decimalPlaces);
+		static e_type	isFloatOrDouble(std::string in, int &decimalPlaces);
 		static bool	isInt(std::string in);
 		static void convert_char(u_cifd value);
 		static void convert_int(u_cifd value);
-		static void convert_float(u_cifd value);
-		static void convert_double(u_cifd value);
+		static void convert_float(u_cifd value, int &decimalPlaces);
+		static void convert_double(u_cifd value, int &decimalPlaces);
 
 	public:
 		static void convert(std::string input);
--- ex00/src/ScalarConverter.cpp
+++ ex00/src/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: pstrohal <pstrohal@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/19 22:09:13 by pstrohal          #+#    #+#             */
-/*   Updated: 2025/01/06 18:29:57 by pstrohal         ###   ########.fr       */
+/*   Updated: 2025/01/07 16:26:59 by pstrohal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,21 +43,21 @@ bool ScalarConverter::isInt(std::string in)
 	}
 	catch (std::exception &e)
 	{
-		std::cout <<e.what()<<std::endl;
+		// std::cout <<e.what()<<std::endl;
 		return false;
 	}
 	
 	return true ;
 }
 
-e_type ScalarConverter::isFloatOrDouble(std::string in)
+e_type ScalarConverter::isFloatOrDouble(std::string in, int &decimalPlaces)
 {
 	int dotCount = 0;
 	size_t len = in.length();
 	bool fEnd = false;
-	if ((in == "+inff") || (in == "-inff") || (in == "nanf"))
+	if ((in == "+inff") || (in == "inff") ||(in == "-inff") || (in == "nanf"))
 		return(FLOAT);
-	else if ((in == "+inf") || (in == "-inf") || (in == "nan"))
+	else if ((in == "+inf") || (in == "inf") ||(in == "-inf") || (in == "nan"))
 		return (DOUBLE);
 	for(size_t i = 0; i < len; i++)
 	{
@@ -74,29 +74,26 @@ e_type ScalarConverter::isFloatOrDouble(std::string in)
 					continue ;
 			}
 			else if (i == len - 1 && in[i] == 'f'){
+				if (!decimalPlaces)
+					decimalPlaces = 1;
 				fEnd = true ;
 				continue ;
 			}
 			else
 				return INVALID ;
 		}
+		if (dotCount)
+			decimalPlaces++;
 	}
 	if (!dotCount)
 		return INVALID;
 	if (fEnd == true)
-	{
-		try {std::stof(in);}
-		catch (...) {return INVALID;}
 		return FLOAT ;
-	}
-	try {std::stod(in);}
-	catch (...){return INVALID;}
 	return DOUBLE ;
-	
 }
 
 
-e_type	ScalarConverter::check_type(std::string input)
+e_type	ScalarConverter::check_type(std::string input, int &decimalPlaces)
 {
 	e_type type = INVALID;
 	
@@ -105,7 +102,7 @@ e_type	ScalarConverter::check_type(std::string input)
 	else if (isInt(input))
 		type = INTEGER;
 	else
-		type = isFloatOrDouble(input);
+		type = isFloatOrDouble(input, decimalPlaces);
 	return type;
 }
 
@@ -118,21 +115,40 @@ std::string to_char(int i)
 
 void ScalarConverter::convert_char(u_cifd value)
 {
-	std::cout<<"char: "<< value.c<<std::endl;
-	std::cout<<"int: "<< static_cast<int>(value.c)<<std::endl;
-	std::cout<<"float: "<< std::fixed<<std::setprecision(1)<< static_cast<float>(value.c)<<"f"<<std::endl;
-	std::cout<<"double: "<< std::fixed<<std::setprecision(1)<< static_cast<double>(value.c)<<std::endl;
+	std::cout	<< std::fixed<<std::setprecision(1)
+				<<"char: "		<< value.c<<std::endl
+				<<"int: "		<< static_cast<int>(value.c)<<std::endl
+				<<"float: "		<< static_cast<float>(value.c)<<"f"<<std::endl
+				<<"double: "	<< static_cast<double>(value.c)<<std::endl;
+	std::cout.unsetf(std::ios::fixed);	//Unset fixed-point formatting
+	std::cout.precision(6);				//Reset precision to default
 }
 void ScalarConverter::convert_int(u_cifd value)
 {
+	std::cout	<< std::fixed<<std::setprecision(1);
+	if (value.i > std::numeric_limits<char>::max() || value.i < std::numeric_limits<char>::min())
+			std::cout<<"char: impossible"<<std::endl;
+	else
+		std::cout<<"char: "<< to_char(static_cast<char>(value.i)) <<std::endl;
+	std::cout	<<"int: "<< value.i <<std::endl;
+	
+	std::cout	<<"float: "		<< static_cast<float>(value.i)<<"f"<<std::endl
+				<<"double: "	<< static_cast<double>(value.i)<<std::endl;
 
-	std::cout<<"char: "<< to_char(static_cast<char>(value.i)) <<std::endl;
-	std::cout<<"int: "<< value.i <<std::endl;
-	std::cout<<"float: "<< std::fixed<<std::setprecision(1)<< static_cast<float>(value.i)<<"f"<<std::endl;
-	std::cout<<"double: "<< std::fixed<<std::setprecision(1)<< static_cast<double>(value.i)<<std::endl;
+	std::cout.unsetf(std::ios::fixed);	//Unset fixed-point formatting
+	std::cout.precision(6);				//Reset precision to default
 }
-void ScalarConverter::convert_float(u_cifd value)
+void ScalarConverter::convert_float(u_cifd value, int &decimalPlaces)
 {
+	if (!std::fmod(value.f, static_cast<int>(value.f)))
+		std::cout	<<std::fixed<<std::setprecision(1);
+	else
+	{
+		if (decimalPlaces > std::numeric_limits<float>::max_digits10)
+			std::cout<<std::fixed<<std::setprecision(std::numeric_limits<float>::max_digits10);
+		else
+			std::cout<<std::fixed<<std::setprecision(decimalPlaces);
+	}
 	if (std::isnan(value.f) || std::isinf(value.f))
 	{
 		std::cout<<"char: impossible"<<std::endl;
@@ -140,88 +156,100 @@ void ScalarConverter::convert_float(u_cifd value)
 	}
 	else
 	{
+		//printing CHAR and check for overflow
 		if (value.f > std::numeric_limits<char>::max() || value.f < std::numeric_limits<char>::min())
 			std::cout<<"char: impossible"<<std::endl;
 		else
 			std::cout<<"char: "<<to_char(static_cast<char>(value.f))<<std::endl;
+		//printing INTEGER and check for overflow
 		if (value.f > (std::numeric_limits<int>::max()) || value.f < std::numeric_limits<int>::min())
 			std::cout<<"int: impossible"<<std::endl;
 		else
 			std::cout<<"int: "<< static_cast<int>(value.f)<<std::endl;
 	}
-	if (!std::fmod(value.f, static_cast<int>(value.f)))
-	{
-		std::cout	<<std::fixed<<std::setprecision(1)
-					<<"float: "<< value.f <<"f\n"
+	
+	
+		std::cout	<<"float: "<< value.f <<"f\n"
 					<<"double: "<< static_cast<double>(value.f)<<std::endl;
-	}
-	else
-	{
-		std::cout
-			<<"float: "<< value.f <<"f\n"
-			<<"double: "<< static_cast<double>(value.f)<<std::endl;
-	}
-
+	std::cout.unsetf(std::ios::fixed);	//Unset fixed-point formatting
+	std::cout.precision(6);				//Reset precision to default
 }
-void ScalarConverter::convert_double(u_cifd value)
+void ScalarConverter::convert_double(u_cifd value, int &decimalPlaces)
 {
+	std::cout<<std::fixed;
+	//chaeck for NAN or INF
 	if (std::isnan(value.d) || std::isinf(value.d))
 	{
 		std::cout<<"char: impossible"<<std::endl;
 		std::cout<<"int: impossible"<<std::endl;
+		std::cout	<<"float: "<< static_cast<float>(value.d)<<"f"<<std::endl;
 	}
 	else
 	{
+		//printing CHAR and check for overflow
 		if (value.d > std::numeric_limits<char>::max() || value.d < std::numeric_limits<char>::min())
 			std::cout<<"char: impossible"<<std::endl;
 		else
 			std::cout<<"char: "<<to_char(static_cast<char>(value.d))<<std::endl;
+
+		//printing INTEGER and check for overflow
 		if (value.d > (std::numeric_limits<int>::max()) || value.d < std::numeric_limits<int>::min())
 			std::cout<<"int: impossible"<<std::endl;
 		else
-			std::cout<<"int: "<< static_cast<int>(value.f)<<std::endl;
-	}
-	if (!std::fmod(value.d, static_cast<int>(value.d)))
-	{
-		if (value.d > (std::numeric_limits<float>::max()) || value.d < std::numeric_limits<float>::min())
-			std::cout<<"float: impossible"<<std::endl;
+			std::cout<<"int: "<< static_cast<int>(value.d)<<std::endl;
+
+		if (decimalPlaces > std::numeric_limits<float>::max_digits10)
+			std::cout<<std::setprecision(std::numeric_limits<float>::max_digits10);
+		else if (!std::fmod(value.d, static_cast<int>(value.d)))
+			std::cout	<<std::fixed<<std::setprecision(1);
 		else
-			std::cout<<"float: "<< static_cast<float>(value.d)<<".0f" <<std::endl;
-		std::cout	<<"double: "<< value.d << ".0"<< std::endl;
+			std::cout<<std::setprecision(decimalPlaces);
+		if (value.d > (std::numeric_limits<float>::max()) || value.d < std::numeric_limits<float>::lowest())
+				std::cout<<"float: impossible"<<std::endl;
+		else
+			std::cout	<<"float: "<< static_cast<float>(value.d)<<"f"<<std::endl;
 	}
+	if (decimalPlaces > std::numeric_limits<double>::max_digits10)
+		std::cout<<std::setprecision(std::numeric_limits<double>::max_digits10);
+	else if (!std::fmod(value.d, static_cast<int>(value.d)))
+		std::cout	<<std::fixed<<std::setprecision(1);
 	else
-	{
-		if (value.d > (std::numeric_limits<float>::max()) || value.d < std::numeric_limits<float>::min())
-			std::cout<<"float: impossible"<<std::endl;
-		else
-			std::cout<<"float: "<< static_cast<float>(value.d)<<"f"<<std::endl;
+		std::cout<<std::setprecision(decimalPlaces);
 		std::cout<<"double: "<< value.d <<std::endl;
-	}
+		std::cout.unsetf(std::ios::fixed);	//Unset fixed-point formatting
+		std::cout.precision(6);				//Reset precision to default
 }
 
 void ScalarConverter::convert(std::string input)
 {
+	try{
 	u_cifd value;
-	e_type type = check_type(input);
-	switch (type)
+	int decimalPlaces = 0;
+		e_type type = check_type(input, decimalPlaces);
+		switch (type)
+		{
+			case CHAR:
+				value.c = static_cast<char>(input[0]);
+				convert_char(value);
+				break;
+			case INTEGER:
+				value.i = std::stoi(input);
+				convert_int(value);
+				break;
+			case FLOAT:
+				value.f = std::stof(input);
+				convert_float(value, decimalPlaces);
+				break;
+			case DOUBLE:
+				value.d = std::stod(input);
+				convert_double(value, decimalPlaces);
+				break;
+			default:
+				throw "invalid";
+		}
+	}
+	catch (...)
 	{
-		case CHAR:
-			value.c = static_cast<char>(input[0]);
-			convert_char(value);
-			break;
-		case INTEGER:
-			value.i = std::stoi(input);
-			convert_int(value);
-			break;
-		case FLOAT:
-			value.f = std::stof(input);
-			convert_float(value);
-			break;
-		case DOUBLE:
-			value.d = std::stod(input);
-			convert_double(value);
-			break;
-		default:
-			std::cout<<"INVALID INPUT!"<<std::endl;
+		std::cout<<"INVALID INPUT!"<<std::endl;
 	}
 }
